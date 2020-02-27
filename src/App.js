d4b725b153a17256ca5b56fe20b6b62a3b40a4d7
Sebastian Sauerer <Sebastian.sauerer@outlook.com>
2020-02-27 23:14:09 +0100

    adding signin component without functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header-component';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
